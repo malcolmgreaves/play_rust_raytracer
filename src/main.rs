483e0643966229382f1dce273007c7c2f379b009
Malcolm Greaves <greaves.malcolm@gmail.com>
2017-04-10 15:44:39 -0700

    first rust code

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+pub struct Color {
+    pub red: f32,
+    pub green: f32,
+    pub blue: f32,
+}
+
+pub struct Sphere {
+    pub center: Point,
+    pub radius: f64,
+    pub color: Color,
+}
+
+pub struct Scene {
+    pub width: u32,
+    pub height: u32,
+    pub fov: f64,
+    pub sphere: Sphere,
+}
