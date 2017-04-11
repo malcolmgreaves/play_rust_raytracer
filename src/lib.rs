631ac691ab58fc7dfc7604d0580f6265e35cfa2c
Malcolm Greaves <greaves.malcolm@gmail.com>
2017-04-11 10:46:24 -0700

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -39,27 +39,55 @@ pub fn render(scene: &Scene) -> DynamicImage {
 }
 
 
-// pub struct Ray {
-//     pub origin: Point,
-//     pub direction: Vector3,
-// }
+#[derive(Copy, Clone, Debug, Deserialize)]
+#[repr(C)]
+pub struct Vector3 {
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
+}
 
-// impl Ray {
-//     pub fn create_prime(x: u32, y: u32, scene: &Scene) -> Ray {
-//         let sensor_x = ((x as f64 + 0.5) / scene.width as f64) * 2.0 - 1.0;
-//         let sensor_y = 1.0 - ((y as f64 + 0.5) / scene.height as f64) * 2.0;
+impl Vector3 {
+    pub fn norm(&self) -> f64 {
+        (self.x * self.x + self.y * self.y + self.z * self.z)
+    }
+}
 
-//         Ray {
-//             origin: Point::zero(),
-//             direction: Vector3 {
-//                     x: sensor_x,
-//                     y: sensor_y,
-//                     z: -1.0,
-//                 }
-//                 .normalize(),
-//         }
-//     }
-// }
+pub struct Ray {
+    pub origin: Point,
+    pub direction: Vector3,
+}
+
+impl Ray {
+    pub fn create_prime(x: u32, y: u32, scene: &Scene) -> Ray {
+        assert!(scene.width >= scene.height);
+        let fov_adjustment = (scene.fov.to_radians() / 2.0).tan();
+        let aspect_ratio = (scene.width as f64) / (scene.height as f64);
+        let sensor_x = ((((x as f64 + 0.5) / scene.width as f64) * 2.0 - 1.0) * aspect_ratio) *
+                       fov_adjustment;
+        let sensor_y = (1.0 - ((y as f64 + 0.5) / scene.height as f64) * 2.0) * fov_adjustment;
+
+        Ray {
+            origin: Point::zero(),
+            direction: Vector3 {
+                    x: sensor_x,
+                    y: sensor_y,
+                    z: -1.0,
+                }
+                .normalize(),
+        }
+    }
+}
+
+pub trait Intersectable {
+    fn intersect(&self, ray: &Ray) -> bool;
+}
+
+impl Intersectable for Sphere {
+    fn intersect(&self, ray: &Ray) -> bool {
+        false
+    }
+}
 
 #[test]
 fn test_can_render_scene() {
