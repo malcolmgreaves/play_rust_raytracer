cfd6ab0d9e66a416e51b4e493322e0db9e33f1b1
Malcolm Greaves <greaves.malcolm@gmail.com>
2017-04-10 21:22:22 -0700

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -39,27 +39,27 @@ pub fn render(scene: &Scene) -> DynamicImage {
 }
 
 
-pub struct Ray {
-    pub origin: Point,
-    pub direction: Vector3,
-}
+// pub struct Ray {
+//     pub origin: Point,
+//     pub direction: Vector3,
+// }
 
-impl Ray {
-    pub fn create_prime(x: u32, y: u32, scene: &Scene) -> Ray {
-        let sensor_x = ((x as f64 + 0.5) / scene.width as f64) * 2.0 - 1.0;
-        let sensor_y = 1.0 - ((y as f64 + 0.5) / scene.height as f64) * 2.0;
+// impl Ray {
+//     pub fn create_prime(x: u32, y: u32, scene: &Scene) -> Ray {
+//         let sensor_x = ((x as f64 + 0.5) / scene.width as f64) * 2.0 - 1.0;
+//         let sensor_y = 1.0 - ((y as f64 + 0.5) / scene.height as f64) * 2.0;
 
-        Ray {
-            origin: Point::zero(),
-            direction: Vector3 {
-                    x: sensor_x,
-                    y: sensor_y,
-                    z: -1.0,
-                }
-                .normalize(),
-        }
-    }
-}
+//         Ray {
+//             origin: Point::zero(),
+//             direction: Vector3 {
+//                     x: sensor_x,
+//                     y: sensor_y,
+//                     z: -1.0,
+//                 }
+//                 .normalize(),
+//         }
+//     }
+// }
 
 #[test]
 fn test_can_render_scene() {
